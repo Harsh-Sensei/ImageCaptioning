bebee927daba393791b06d33a439fa6394d183c9
Harsh-Sensei <prodigy.harsh18@gmail.com>
2021-12-20 11:31:12 +0530

    Text encoder low accuracy made

--- CustomDatasets.py
+++ CustomDatasets.py
@@ -4,50 +4,135 @@
 import PIL.Image as Image
 import numpy
 import os
+import torch
 from torch.utils.data import Dataset
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import DataLoader, Dataset
 import json
 import spacy
 
-nlp = spacy.load()
+spacy_eng = spacy.load("en_core_web_lg")
+
+
+class Vocabulary:
+    def __init__(self, freq_threshold):
+        self.freq_threshold = freq_threshold
+        self.itos = {0: "<PAD>", 1: "<SOS>", 2: "<EOS>", 3: "<UKN>"}
+        self.stoi = {"<PAD>": 0, "<SOS>": 1, "<EOS>": 2, "<UKN>": 3}
+
+    def __len__(self):
+        return len(self.itos)
+
+    @staticmethod
+    def tokenize(text):
+        return [tok.text.lower() for tok in spacy_eng.tokenizer(text)]
+
+    def build_vocabulary(self, sentence_list):
+        freq = {}
+        idx = 4
+
+        for elem in sentence_list:
+            for word in self.tokenize(elem):
+                if word not in freq:
+                    freq[word] = 1
+                else:
+                    freq[word] += 1
+
+                if freq[word] == self.freq_threshold:
+                    self.stoi[word] = idx
+                    self.itos[idx] = word
+                    idx += 1
+
+        return
+
+    def numericalize(self, text):
+        tokenized_text = self.tokenize(text)
+
+        return [
+            self.stoi[token] if token in self.stoi else self.stoi["<UKN>"]
+            for token in tokenized_text
+        ]
 
 
 class UCM_Captions(Dataset):
 
-    def __init__(self, transform=None, ret_type="image-image"):
+    def __init__(self, transform=None, ret_type="image-image", freq_threshold=2):
         self.transform = transform
         self.ret_type = ret_type
         self.root_dir = "./dataset/UCM_Captions/UCM_captions/imgs"
-        self.meta_data = self.getMetaData()
+        self.num_captions_per_img, self.meta_data = self.getMetaData()
         self.image_names = list(self.meta_data.keys())
+
+        # Example of each elem in captions
+        # [{'tokens': ['There', 'is', 'a', 'piece', 'of', 'farmland'], 'raw': 'There is a piece of farmland .',
+        #  'imgid': 0, 'sentid': 0}, {'tokens':.........]
+
         self.captions = list(self.meta_data.values())
+        self.captions_raw = self.getRawCaptions(self.captions)
+        self.freq_threshold = freq_threshold
+        self.vocab = Vocabulary(self.freq_threshold)
+        self.vocab.build_vocabulary(self.captions_raw)
 
     def getMetaData(self):
         data_info = open(r"./dataset/UCM_Captions/UCM_captions/dataset.json")
         data_info = json.load(data_info)['images']
+        # data_info is a list of dict and each dict contains filename, sentences, tokens, etc of each image
+
+        captions_per_img = len(data_info[0]['sentids'])
 
         result = {}
         for elem in data_info:
             result[str(elem['filename'])] = elem['sentences']
 
-        return result
+        return captions_per_img, result
 
     def __len__(self):
-        return len(self.meta_data)
+        if self.ret_type == "image-image" or self.ret_type == "image-caption":
+            return len(self.meta_data)
+        else:
+            return self.num_captions_per_img * len(self.meta_data)
 
     def __getitem__(self, index):
-        img_path = os.path.join(self.root_dir, self.image_names[index])
-        image = Image.open(img_path)
-        y_label = self.captions[index]
+
+        if self.ret_type == "image-image":
+            img_path = os.path.join(self.root_dir, self.image_names[index])
+            image = Image.open(img_path)
+            y_label = self.captions[index]
+        elif self.ret_type == "image-caption":
+            y_label = self.captions[index]
+        else:
+            y_label = self.captions[int(index) // int(self.num_captions_per_img)]
+            y_label = y_label[int(index) % int(self.num_captions_per_img)]['raw']
+            y_label = self.numericalize_caption(y_label)
+            # print("y_label")
+            # print(y_label)
+            y_label = torch.tensor(y_label)
+
+
 
         if self.transform:
             image = self.transform(image)
 
         if self.ret_type == "image-image":
-            return (image, image)
+            return image, image
         elif self.ret_type == "image-caption":
-            return (image, y_label)
+            return image, y_label
         elif self.ret_type == "caption-caption":
-            return (y_label, y_label)
+            return y_label, y_label
+
+    def numericalize_caption(self, caption):
+        result = [self.vocab.stoi["<SOS>"]]
+        result += self.vocab.numericalize(caption)
+        result += [self.vocab.stoi["<EOS>"]]
+        return result
+
+    def getRawCaptions(self, captions):
+        result = []
+        for elem in captions:
+            for cap in elem:
+                result.append(cap['raw'])
+
+        return result
 
 
 class Sydney_Captions(Dataset):
@@ -87,6 +172,28 @@ class Sydney_Captions(Dataset):
             return (y_label, y_label)
 
 
+class AuxPadClass:
+    def __init__(self, pad_idx):
+        self.pad_idx = pad_idx
+
+    def __call__(self, batch):
+        items = [item[0] for item in batch]
+        input = pad_sequence(items, batch_first=True, padding_value=self.pad_idx)
+        return input, input
+
+
+def getTextUCMDataLoader(batch_size=32):
+    dataset = UCM_Captions(transform=None, ret_type="caption-caption")
+    UCM_train_set, UCM_test_set = torch.utils.data.random_split(dataset,
+                                                                [int(dataset.__len__() * 0.8), dataset.__len__() -
+                                                                 int(dataset.__len__() * 0.8)])
+    print(dataset.vocab.itos)
+    pad_idx = dataset.vocab.stoi["<PAD>"]
+    TrainLoader = DataLoader(UCM_train_set, batch_size=batch_size, collate_fn=AuxPadClass(pad_idx=pad_idx))
+    TestLoader = DataLoader(UCM_test_set, batch_size=batch_size, collate_fn=AuxPadClass(pad_idx=pad_idx))
+
+    return TrainLoader, TestLoader, pad_idx, dataset.vocab.__len__()
+
+
 if __name__ == "__main__":
-    sample = UCM_Captions()
-    print(sample.captions)
+    A = getTextUCMDataLoader()
--- textEncoder.py
+++ textEncoder.py
@@ -8,139 +8,160 @@ from torch.utils.data import DataLoader  # For dataset management
 import torchvision.datasets as datasets  # Standard datasets, has COCO
 import torchvision.transforms as transforms  # transformations on dataset
 import torchvision.models as models
-from CustomDatasets import Sydney_Captions, UCM_Captions  # Datasets involving captions
+from CustomDatasets import *  # Datasets involving captions
 import numpy as np
 import spacy
-
-# Hyper parameters
-learning_rate = 0.001
-embedding_dim = 300
-encoded_text_dim = 1000
-encoder_hidden_size = 512
-encoder_num_layers = 2
-decoder_hidden_size = 300
-decoder_num_layers = 1
-batch_size = 32
-num_epochs = 10
 
 # Device setup for runtime
 device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
 
-# loading the UCM_Captions and Sydney_Captions datasets
-UCM_dataset = UCM_Captions(ret_type="caption-caption")
-Sydney_dataset = Sydney_Captions(ret_type="caption-caption")
 
-UCM_len = UCM_dataset.__len__()
-Sydney_len = Sydney_dataset.__len__()
+class TextEncoder(nn.Module):
 
-# Setting up training and testing data
-UCM_train_set, UCM_test_set = torch.utils.data.random_split(UCM_dataset,
-                                                            [int(UCM_len * 0.8), UCM_len - int(UCM_len * 0.8)])
-Sydney_train_set, Sydney_test_set = torch.utils.data.random_split(UCM_dataset, [int(Sydney_len * 0.8),
-                                                                                Sydney_len - int(Sydney_len * 0.8)])
+    def __init__(self, embedding_dim=300, hidden_size=512, num_layers=2, vocab_size=1000, output_dim=1000, p=0.5):
+        super(TextEncoder, self).__init__()
 
-# Initializing dataloader
-UCM_train_loader = DataLoader(dataset=UCM_train_set, batch_size=batch_size, shuffle=True)
-UCM_test_loader = DataLoader(dataset=UCM_test_set, batch_size=batch_size, shuffle=True)
-Sydney_train_loader = DataLoader(dataset=Sydney_train_set, batch_size=batch_size, shuffle=True)
-Sydney_test_loader = DataLoader(dataset=Sydney_test_set, batch_size=batch_size, shuffle=True)
+        self.encoded_features = None
+        self.embedding_dim = embedding_dim
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.output_dim = output_dim
+        self.vocab_size = vocab_size
+        self.embed = nn.Embedding(vocab_size, embedding_dim)
+
+        self.dropout = nn.Dropout(p)
+
+        # Encoder LSTM
+        self.encoderLSTM = nn.LSTM(input_size=self.embedding_dim,
+                                   hidden_size=self.hidden_size,
+                                   num_layers=self.num_layers,
+                                   dropout=p,
+                                   batch_first=True)
+
+        self.linearProject = nn.Linear(self.hidden_size, output_dim)
+
+    def forward(self, x):
+        x = self.dropout(self.embed(x))
+        init_hidden_state = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        init_cell_state = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        outputs, (hidden_states, _) = self.encoderLSTM(x, (init_hidden_state, init_cell_state))
+        # dim outputs = (batch_size, sequence_len, hidden_size)
+
+        encoded_text = self.linearProject(outputs[:, -1, :])
+        # print("outputs.shape")
+        # print(outputs.shape)
+
+        return encoded_text
+
+
+class TextDecoder(nn.Module):
+    def __init__(self, feature_dim=1000, embedding_dim=300, vocab_size=1000, num_layers=2, dropout=0.5):
+        super(TextDecoder, self).__init__()
+        self.hidden_size = vocab_size
+        self.vocab_size = vocab_size
+        self.num_layers = num_layers
+        self.feature_dim = feature_dim
+        # Decoder LSTM
+        self.decoderLSTM = nn.LSTM(input_size=self.feature_dim,
+                                   hidden_size=self.vocab_size,
+                                   num_layers=self.num_layers,
+                                   dropout=dropout,
+                                   batch_first=True)
+
+    def forward(self, features, captions):
+        batch_size = features.shape[0]
+        target_len = captions.shape[1]
+        target_vocab_size = self.vocab_size
+
+        outputs = torch.zeros(batch_size, target_len, target_vocab_size)
+
+        hidden = torch.zeros(self.num_layers, batch_size, self.hidden_size).to(device=device)
+        cell = torch.zeros(self.num_layers, batch_size, self.hidden_size).to(device=device)
+
+        # dim features = (batch_size, feature_dim)
+        features = features.unsqueeze(1)
+
+        for t in range(target_len):
+            output, (hidden, cell) = self.decoderLSTM(features, (hidden, cell))
+            if t == 0:
+                pass
+                # print("output_shape")
+                # print(output.shape)
+            outputs[:, t, :] = output.squeeze(1)
+
+        return outputs
 
 
 class TextEncoderDecoder(nn.Module):
 
-    def __init__(self, dropout=0.5):
+    def __init__(self, feature_dim, embedding_dim, en_hidden_size, en_num_layers, de_num_layers, vocab_size, p=0.5):
         super(TextEncoderDecoder, self).__init__()
 
-        # Hyper parameters
-        self.encoded_features = None
-        self.embedding_dim = embedding_dim
-        self.encoder_hidden_size = encoder_hidden_size
-        self.encoder_num_layers = encoder_num_layers
-        self.encoded_text_dim = encoded_text_dim
-        self.decoder_hidden_size = decoder_hidden_size
-        self.decoder_num_layers = decoder_num_layers
-        self.bidirectional_encoder = True
+        self.encoder = TextEncoder(embedding_dim=embedding_dim,
+                                   hidden_size=en_hidden_size,
+                                   num_layers=en_num_layers,
+                                   output_dim=feature_dim,
+                                   vocab_size=vocab_size,
+                                   p=p)
 
-        # Dropout
-        self.dropout = nn.Dropout(dropout)
-
-        # Encoder LSTM
-        self.encoderLSTM = nn.LSTM(input_size=self.embedding_dim,
-                                   hidden_size=self.encoder_hidden_size,
-                                   num_layers=self.encoder_num_layers,
-                                   dropout=dropout,
-                                   bidirectional=self.bidirectional_encoder,
-                                   batch_first=True)
-        self.linearProject = nn.Linear(self.encoder_hidden_size, encoded_text_dim)
-
-        # Decoder LSTM
-        self.decoderLSTM = nn.LSTM(input_size=self.encoded_text_dim,
-                                   hidden_size=self.decoder_hidden_size,
-                                   num_layers=self.decoder_num_layers,
-                                   dropout=dropout,
-                                   batch_first=True)
-
+        self.decoder = TextDecoder(feature_dim=feature_dim,
+                                   embedding_dim=embedding_dim,
+                                   vocab_size=vocab_size,
+                                   num_layers=de_num_layers,
+                                   dropout=p)
 
     def forward(self, x):
-        # dim x = (batch_size, sequence_len, embedding_dim)
-        print(x.shape)
-        init_hidden_state_encoder = torch.zeros(self.encoder_num_layers, x.size(0), self.encoder_hidden_size).to(device)
-        init_cell_state_encoder = torch.zeros(self.encoder_num_layers, x.size(0), self.encoder_hidden_size).to(device)
-        outputs, (hidden_states, _) = self.encoderLSTM(x, (init_hidden_state_encoder, init_cell_state_encoder))
-        # dim outputs = (batch_size, sequence_len, hidden_size)
+        # dim x = (batch_size, sequence_len)
 
-        encoded_text = self.linearProject(outputs[:, -1, :])
-        self.encoded_features = encoded_text
+        features = self.encoder(x)
+        prediction = self.decoder(features, x)
 
-        print(outputs.shape)
-
-        init_hidden_state_decoder = torch.zeros(self.decoder_num_layers, x.size(0), self.decoder_hidden_size).to(device)
-        init_cell_state_decoder = torch.zeros(self.decoder_num_layers, x.size(0), self.decoder_hidden_size).to(device)
-
-        hidden = init_hidden_state_decoder
-        cell = init_cell_state_decoder
-
-        encoded_text = encoded_text.unsqueeze(1)
-
-        print(encoded_text.shape)
-
-        for i in range(x.size(1)):
-
-            output, (hidden, cell) = self.decoderLSTM(encoded_text, hidden, cell)
-            # dim output = (N, 1, decoder_hidden_size)
-            print(output.shape)
-
-            output.unsqueeze(1)
-            # dim output = (N, 1, decoder_hidden_size)
+        return prediction
 
 
+if __name__ == "__main__":
 
+    # Hyper parameters for learning
+    learning_rate = 0.001
+    batch_size = 32
+    num_epochs = 10
 
+    # Hyper parameters for network
+    feature_dim = 1000
+    en_num_layers = 2
+    de_num_layers = 2
+    en_hidden_size = 512
+    embedding_dim = 300
+    vocab_size = None  # to be defined after datasets are loaded
+    dropout_p = 0.5
 
+    # Initializing dataloader
+    UCM_train_loader, UCM_test_loader, pad_idx, vocab_size = getTextUCMDataLoader(batch_size=batch_size)
 
+    model = TextEncoderDecoder(feature_dim=feature_dim,
+                               embedding_dim=embedding_dim,
+                               en_hidden_size=en_hidden_size,
+                               en_num_layers=en_num_layers,
+                               de_num_layers=de_num_layers,
+                               vocab_size=vocab_size,
+                               p=dropout_p).to(device=device)
 
+    criterion = nn.CrossEntropyLoss(ignore_index=pad_idx)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-        return x
+    loss_vector = []
+    outputs = []
 
+    for epoch in range(num_epochs):
+        for (data, ground_truth) in UCM_train_loader:
+            data = data.to(device=device)
+            ground_truth = ground_truth.to(device=device)
+            output = model(data)
+            output = output.permute(0, 2, 1).to(device=device)
+            loss = criterion(output, ground_truth)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-model = TextEncoderDecoder()
-criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
-loss_vector = []
-outputs = []
-for epoch in range(num_epochs):
-    for (data, ground_truth) in UCM_train_loader:
-        data = data.to(device=device)
-        ground_truth = ground_truth.to(device=device)
-        # print("data_shape:")
-        # print(data.shape)
-        output = model(data)
-        loss = criterion(output, ground_truth)
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-    print(f'Epoch:{epoch + 1}, Loss:{loss.item():.4f}')
-    outputs.append([epoch, data, output, loss.item()])
+        print(f'Epoch:{epoch + 1}, Loss:{loss.item():.4f}')
+        outputs.append([epoch, data, output, loss.item()])
